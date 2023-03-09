9721c1c7f580979dc485ce7907148b610bd2e5a2
adedotxn <adedotxn.dev@gmail.com>
2023-03-09 01:48:13 +0100

    adjusted deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://felabs.github.io',
-  base: '/creative-frontend',
+    site: 'https://adedotxn.github.io',
+  base: '/fe-labs',
 });
